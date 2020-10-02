424805a7e7ecfcbc06069749735625f9ce9ab767
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-10-02 23:17:10 +0200

    missing js

---
+++ main.js
@@ -0,0 +1,7 @@
+const sharingPannel = document.querySelector(".sharing-pannel");
+
+console.log(sharingPannel);
+
+sharingPannel.addEventListener("click", function() {
+  sharingPannel.nav.style.display = 'flex';
+});
