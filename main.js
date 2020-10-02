e9eb2163bee21990c8e391c6ae8e3e86a8ef6d68
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-10-03 00:38:53 +0200

    add js

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
+const sharingButton = document.querySelector(".sharing-btn");
 const sharingPannel = document.querySelector(".sharing-pannel");
+// const invisibleSquare = document.querySelector(".invisible-square");
+// const invisibleElement = document.querySelector(".invisible-element");
 
-console.log(sharingPannel);
+sharingButton.addEventListener("click", function(){
+  sharingPannel.style.display = 'flex';
+  // invisibleSquare.style.display = 'unset';
+  // invisibleElement.style.display = 'unset';
+});
 
-sharingPannel.addEventListener("click", function() {
-  sharingPannel.nav.style.display = 'flex';
+sharingButton.addEventListener("click", function(){
+  sharingPannel.style.display = 'none';
+  // invisibleSquare.style.display = 'none';
+  // invisibleElement.style.display = 'none';
 });
