f226355a5df10ced19b4c26971866f8a431c531f
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-10-04 19:25:22 +0200

    fix hovering sharing btn

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const sharingButton = document.querySelector(".sharing-btn");
 const sharingPannel = document.querySelector(".sharing-pannel");
+const sharingBtn = document.querySelector(".sharing-btn");
 
 sharingButton.addEventListener('mouseover', () => {
   sharingPannel.style.display = 'none';
@@ -8,11 +9,15 @@ sharingButton.addEventListener('mouseover', () => {
 sharingButton.addEventListener("mouseover", () => {
   if (sharingPannel.style.display === 'none') {
     sharingPannel.style.display = 'flex';
+    sharingButton.style.backgroundColor = 'var(--very-dark-grayish-blue)';
+    sharingButton.style.color = 'var(--light-grayish-blue)';
   }
 });
 
 sharingPannel.addEventListener('mouseleave', () => {
   sharingPannel.style.display = 'none';
+  sharingButton.style.backgroundColor = 'var(--light-grayish-blue)';
+  sharingButton.style.color = 'var(--very-dark-grayish-blue)';
 });
 
 const displayPannelModileVp = () => {
