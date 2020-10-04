f0bda9b58440314fa3759b7cc067fd4fc22d79fa
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-10-04 18:49:17 +0200

    fix JS and do media queries

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 const sharingButton = document.querySelector(".sharing-btn");
 const sharingPannel = document.querySelector(".sharing-pannel");
 
-
-console.log('coucou');
-
-sharingButton.addEventListener("click", () => {
-  sharingPannel.style.display = 'flex';
-
-});
-
-sharingButton.addEventListener("click", () => {
+sharingButton.addEventListener('mouseover', () => {
   sharingPannel.style.display = 'none';
-
 });
+
+sharingButton.addEventListener("mouseover", () => {
+  if (sharingPannel.style.display === 'none') {
+    sharingPannel.style.display = 'flex';
+  }
+});
+
+sharingPannel.addEventListener('mouseleave', () => {
+  sharingPannel.style.display = 'none';
+});
+
+const displayPannelModileVp = () => {
+  if (window.innerWidth <= 900) {
+    sharingButton.addEventListener("click", () => {
+      if (sharingPannel.style.display === 'none') {
+        sharingPannel.style.display = 'flex';
+      } else {
+        sharingPannel.style.display = 'none';
+      }
+    });
+  }
+};
+
+displayPannelModileVp();
